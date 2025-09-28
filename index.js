92c1c8b79900b1a1abd882465dcd3e620addaaf1
sheikhrabby97 <sheikhrabby97@gmail.com>
2025-09-28 15:47:14 +0600

    add CRUD funtionality api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -25,6 +25,50 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const coffeesCollection = client.db('coffeeDB').collection('coffees');
+
+
+        app.get('/coffees' , async(req , res)=>{
+            const result = await coffeesCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/coffees/:id', async(req , res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await coffeesCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/coffees', async(req, res)=>{
+            const newCoffee = req.body;
+            console.log(newCoffee);
+            const result = await coffeesCollection.insertOne(newCoffee);
+            res.send(result);
+        })
+
+        app.put('/coffees/:id', async(req , res)=> {
+            const id = req.params.id;
+            const filter = {_id : new ObjectId(id)};
+            const options = {upsert: true};
+            const updateCoffee = req.body;
+            const updatedDoc = {
+                $set : updateCoffee
+            }
+            const result = await coffeesCollection.updateOne(filter , updatedDoc , options);
+
+            res.send(result);
+        })
+
+        app.delete('/coffees/:id', async(req , res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await coffeesCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
